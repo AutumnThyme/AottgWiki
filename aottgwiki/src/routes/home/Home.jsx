41ba0c6d724870bf7a24bc78150bd3e243adc239
AutumnThyme <soboredaottg@gmail.com>
2022-05-09 18:45:31 -0400

    Stopped using react-player due to a bug with styling
    using the html video tag instead.

--- aottgwiki/src/routes/home/Home.jsx
+++ aottgwiki/src/routes/home/Home.jsx
@@ -2,24 +2,29 @@ import "./Home.scss"
 import Navbar from "../../components/navbar/Navbar";
 import ReactPlayer from 'react-player';
 
+const YT_TEST_VIDEO = "https://www.youtube.com/watch?v=rYvQoussA98";
+const LOCAL_TEST_VIDEO = "assets/ClipsOfBunch5.mp4";
+
 export default function Home() {
+  const video = <ReactPlayer
+    className="react-player"
+    url={LOCAL_TEST_VIDEO}
+    controls={false}
+    loop={true}
+    playing={true}
+    muted={true}
+    width='100%'
+    height='1080px'
+  />
+
   return (
     <div className="home-container">
         <Navbar tabOpen="Home"/>
         <div className="player-wrapper">
-          <ReactPlayer
-            className="react-player"
-            url="https://www.youtube.com/watch?v=rYvQoussA98"
-            controls="false"
-            loop="true"
-            playing="true"
-            muted="false"
-            width='100%'
-            height='116vh'
-            config={{ youtube: { playerVars: { disablekb: 1 } } }}
-          />
+        <video class="HeaderSection-video HeaderSection-video--mobile" autoplay="autoplay" loop="loop" muted="muted" playsinline="playsinline" webkit-playsinline="webkit-playsinline" x5-playsinline="x5-playsinline" poster="https://static.playoverwatch.com/img/pages/home/header-video-poster-mobile-0443ef2d5e.jpg" data-js="header-video">
+          <source src={LOCAL_TEST_VIDEO} type="video/mp4" />
+        </video>
         </div>
-        <div className="spacer" />
         <div className="about-section">
           <div className="about-container">
             <h1>About</h1>
