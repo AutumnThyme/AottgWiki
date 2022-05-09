4677efea53c30ca8fd6563044521663d036ed006
AutumnThyme <soboredaottg@gmail.com>
2022-05-09 19:18:32 -0400

    Added both local and youtube video sources

--- aottgwiki/src/routes/home/Home.jsx
+++ aottgwiki/src/routes/home/Home.jsx
@@ -6,7 +6,7 @@ const YT_TEST_VIDEO = "https://www.youtube.com/watch?v=rYvQoussA98";
 const LOCAL_TEST_VIDEO = "assets/ClipsOfBunch5.mp4";
 
 export default function Home() {
-  const video = <ReactPlayer
+  const yt_video_element = <ReactPlayer
     className="react-player"
     url={LOCAL_TEST_VIDEO}
     controls={false}
@@ -17,18 +17,25 @@ export default function Home() {
     height='1080px'
   />
 
+  const local_video_element = <video class="HeaderSection-video HeaderSection-video--mobile" autoplay="autoplay" loop="loop" muted="muted" playsinline="playsinline" webkit-playsinline="webkit-playsinline" x5-playsinline="x5-playsinline" poster="https://static.playoverwatch.com/img/pages/home/header-video-poster-mobile-0443ef2d5e.jpg" data-js="header-video">
+                                <source src={LOCAL_TEST_VIDEO} type="video/mp4" />
+                              </video>
+
   return (
     <div className="home-container">
         <Navbar tabOpen="Home"/>
         <div className="player-wrapper">
-        <video class="HeaderSection-video HeaderSection-video--mobile" autoplay="autoplay" loop="loop" muted="muted" playsinline="playsinline" webkit-playsinline="webkit-playsinline" x5-playsinline="x5-playsinline" poster="https://static.playoverwatch.com/img/pages/home/header-video-poster-mobile-0443ef2d5e.jpg" data-js="header-video">
-          <source src={LOCAL_TEST_VIDEO} type="video/mp4" />
-        </video>
+        {local_video_element}
         </div>
         <div className="about-section">
           <div className="about-container">
             <h1>What is this?</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque eos, placeat magnam ipsum quod incidunt ipsam cupiditate tenetur exercitationem vero excepturi temporibus harum necessitatibus sit ducimus quia assumenda in aut.</p>
+            <p>
+              This is a fan made website for a longstanding community maintained game called the Attack on Titan Tribute Game
+              which is more formally called AOTTG. This game was originally made and maintained by Fenglee but as of recently
+              has become a completely community maintained game. The gameplay this game provides varies from competative modes
+              based on the game's in depth movement mechanics to more casual gameplay with multiplayer.
+            </p>
           </div>
         </div>
     </div>
